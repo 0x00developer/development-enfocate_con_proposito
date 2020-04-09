0a7d2c302e0cff90dc622bd4362e3a0ef6d63b38
Javier Andrés <55107141+JA-programmer@users.noreply.github.com>
2020-04-09 14:25:53 -0500

    Update

--- 0x00/views/noticias-view.php
+++ 0x00/views/noticias-view.php
@@ -1,6 +1,24 @@
 <main class="secondaryMain containerA">
     <section>
         <h2 class="title">noticias</h2>
+        <p>La Fundación Enfócate con Propósito tiene el privilegio de compartir un espacio de formación en ventas a
+            través del Webinar Ventas para jóvenes emprendedores.</p>
+        <p>César Lozano experto en Neuroventas certificado por Jurgen Klaric, compartirá en una 1 hora aspectos
+            fundamentales para que los jóvenes nutran sus proyectos y/o emprendimientos a través de las ventas.</p>
+        <p><cite>"Hoy es cuando más debemos innovar para vender, mientras más creativas sean tus estrategias, más
+                posibilidades de éxito tendrás con ellas"</cite><br>Cesar Lozano Jr.</p>
+        <strong class="subtitle"><a href="https://diegodiaz3.typeform.com/to/M1pi3E"
+                target="_blank">CONFIRMAR</a></strong>
+        <br>
+        <p>Diego Armando Díaz Rincón</p>
+        <br>
+        <strong class="subtitle"><a href="prevencion-covid-19.php">Prevención COVID-19</a></strong>
+    </section>
+    <figure>
+        <img src="images/webinar01-cesar-lozano.jpg" alt="Webinar con César Lozano">
+    </figure>
+    <section>
+        <h2 class="title">noticias</h2>
         <p>La Fundación Enfócate con Propósito participó
             en la campaña <cite>"Hijo ¿Estás pensando en suicidarte?"</cite>
             de la Asociación Colombiana de Psiquiatría.</p>
@@ -12,11 +30,9 @@
             vida.
         </p>
         <p><cite>"vale la pena decirle un SI a la vida así pasemos
-            por grandes tormentas o crisis"</cite>.<br>Diego Armando Díaz Rincón</p>
-        <br>
-        <strong class="subtitle"><a href="prevencion-covid-19.php">Prevención COVID-19</a></strong>
+                por grandes tormentas o crisis"</cite>.<br>Diego Armando Díaz Rincón</p>
     </section>
     <figure>
-        <img src="images/dia-mundial-de-la-prevencion-del-suicidio.jpg" alt="Dí mundial de la prevención del suicidio">
+        <img src="images/dia-mundial-de-la-prevencion-del-suicidio.jpg" alt="Día mundial de la prevención del suicidio">
     </figure>
 </main>
